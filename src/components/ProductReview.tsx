1fd56edb3fa9776e665821ff225c5ff18176c38f
Mahadi Hasan <138366726+Mahadi1000@users.noreply.github.com>
2024-09-03 19:38:40 +0600

    Update ProductReview.tsx

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -1,53 +1,71 @@
-import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { useState } from 'react';
-import { addComment } from '@/redux/features/commentsSlice';
+import {
+  useGetCommentQuery,
+  usePostCommentMutation,
+} from '@/redux/features/products/productApi';
 
+interface IProps {
+  id: string;
+}
 
+export default function ProductReview({ id }: IProps) {
+  const [inputValue, setInputValue] = useState<string>('');
 
-export default function ProductReview({productId}: {productId: number}) {
-  const [comment, setcomment] = useState('')
-  console.log(comment)
-  const dispatch = useAppDispatch()
-  const comments = useAppSelector(state => state.comments.comments.filter(comment => comment.productId === productId))
-  const handleAddComment = ()=>{
-    dispatch(addComment({
-      id: new Date().getTime(),
-      productId,
-      content: comment,
+  const { data } = useGetCommentQuery(id, {
+    refetchOnMountOrArgChange: true,
+    pollingInterval: 30000,
+  });
+  const [postComment, { isLoading, isError, isSuccess }] =
+    usePostCommentMutation();
 
-    }))
-    setcomment('');
-  }
+  console.log(isLoading);
+  console.log(isError);
+  console.log(isSuccess);
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(inputValue);
+
+    const options = {
+      id: id,
+      data: { comment: inputValue },
+    };
+
+    postComment(options);
+    setInputValue('');
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div className="max-w-7xl mx-auto mt-5">
-      <h1 className="mt-5 text-2xl font-semibold py-5">Review</h1>
-      <div className="flex gap-5 items-center">
+      <form className="flex gap-5 items-center" onSubmit={handleSubmit}>
         <Textarea
-          value={comment}
-          onChange={(e) => setcomment(e.target.value)}
           className="min-h-[30px]"
+          onChange={handleChange}
+          value={inputValue}
         />
         <Button
-          onClick={handleAddComment}
+          type="submit"
           className="rounded-full h-10 w-10 p-2 text-[25px]"
         >
           <FiSend />
         </Button>
-      </div>
+      </form>
       <div className="mt-10">
-        {comments.map((comment, index) => (
+        {data?.comments?.map((comment: string, index: number) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
-              <AvatarImage src="https://i.ibb.co/BC10fd9/pxfuel-30.jpg" />
+              <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <p>{comment.content}</p>
+            <p>{comment}</p>
           </div>
         ))}
       </div>
